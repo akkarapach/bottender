b7359632af9c5e05b0fd0b010d0f0036edd30d09
Akkarapach Boonchu -X (aboonchu - CELESTICA HOLDINGS PTE LTD at Cisco) <aboonchu@cisco.com>
2018-09-14 09:16:25 +0700

    first commit

--- linebot/bottender.config.js
+++ linebot/bottender.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   line: {
-    accessToken: '__PUT_YOUR_ACCESS_TOKEN_HERE__',
-    channelSecret: '__PUT_YOUR_CHANNEL_SECRET_HERE__',
+    accessToken:
+      'h0tORMryrSu30vnWr0YBN9SdGUTmXl+qNzYwRnKFcEO3Wv6HVtLPNiwl/CJPGK2cgQSHk255I44a6caJ19nsvS/G7jlDajzCUjnhYJj26/Sulk+6AYE+zRqtDp1rysxPdSKafPd9+XLeenbBRDx1EwdB04t89/1O/w1cDnyilFU=',
+    channelSecret: '4f15b028df960d811bcc39d764187094',
   },
 };
